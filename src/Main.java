6daabaa3f811d1617bff1f235b4295ce4bc38e17
VilleSchulz <villemsc@metropolia.fi>
2024-08-19 11:53:16 +0300

    testi push

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//Tässä on nyt pääohjelma
+/*TOINEN KOMMENTTI*/
+
+public class Main {
+    /*
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Metropoliassa ollaan");
+    }
+
+*/
+public static void main(String[] args) {
+    System.out.println("hahaha");}
+}
