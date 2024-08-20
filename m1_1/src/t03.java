f46def9405b219ba219b535cb7f4b5b17c44a2e8
VilleSchulz <villemsc@metropolia.fi>
2024-08-20 11:51:32 +0300

    t03

--- m1_1/src/t03.java
+++ m1_1/src/t03.java
@@ -14,10 +14,11 @@ import java.util.Scanner;
             System.out.println("Give the third number:");
             double third = Double.parseDouble(scanner.nextLine());
 
+            double sum = first + second + third;
             double product = first*second*third;
-            double average =(first+second+third) / 3;
+            double average = (first+second+third) / 3;
 
-            System.out.println("The sum of the numbers is " + (first + second + third));
+            System.out.println("The sum of the numbers is " + sum);
             System.out.println("The product of the numbers is " + product);
             System.out.println("The average of the numbers is " + average);
 
