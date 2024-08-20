f8bfc1d8b65fd8c96117e47c8c3a61ffe7ae42d3
VilleSchulz <villemsc@metropolia.fi>
2024-08-20 12:43:40 +0300

    t01

--- m1_2/src/t01.java
+++ m1_2/src/t01.java
@@ -6,9 +6,10 @@ public class t01 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a temperature as Fahrenheit");
         double temperatureF = scanner.nextDouble();
+        //Example: (50°F - 32) x .5556 = 10°C
         double temperatureC = (temperatureF - 32)* 0.5556;
         System.out.printf("Here is converted value to Celsius: %.2f ", temperatureC);
         scanner.close();
     }
 }
-//Example: (50°F - 32) x .5556 = 10°C
+
