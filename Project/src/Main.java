75512c1caa910d884d8ae50f858490fdf30a5fb9
VilleSchulz <villemsc@metropolia.fi>
2024-08-21 14:40:56 +0300

    tuntiesimerkki

---
+++ Project/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main starts");
+        Customer customer1 = new Customer();
+
+        for (int i = 0; i <1000; i++){
+            Customer customer2 = new Customer();
+        }
+
+
+    }
+}
