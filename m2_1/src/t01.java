922b9e97e212f1a2a5ffc882d52dad996f4c6aa8
VilleSchulz <villemsc@metropolia.fi>
2024-08-26 14:01:26 +0300

    m2_1 t01 started

--- m2_1/src/t01.java
+++ m2_1/src/t01.java
@@ -1,8 +1,46 @@
 //Task 1: Television class
 //Implement class Television that together with TelevisionViewer
 // produces the following output:
+
 public class t01{
+    public static void main(String[] args){
 
+        
+    }
+    
+}
+
+public class Television{
+
+}
+
+
+
+public class TelevisionViewer {
+    public static void main(String[] args) {
+        Television myTV = new Television();
+        myTV.setChannel(1);
+
+        for (int day = 1; day <= 10; day++) {
+            System.out.println("Woke up, day " + day);
+
+            boolean tired = false;
+
+            if (!myTV.isOn())
+                myTV.pressOnOff();
+
+            while (!tired) {
+                System.out.println("Watching channel " + myTV.getChannel());
+                myTV.setChannel(myTV.getChannel() + 1);
+                if (myTV.getChannel() % 4 == 0)
+                    tired = true;
+            }
+
+            myTV.pressOnOff();
+
+            System.out.println("Falling asleep");
+        }
+    }
 }
 
 
@@ -56,7 +94,3 @@ public class t01{
 
 
 
-
-
-{
-}
