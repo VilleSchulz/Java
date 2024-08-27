4dfb1ad1194684fb82e8558a259b7576b6f3bb59
Ville <villesc@gmail.com>
2024-08-27 21:49:36 +0300

    Orientation 2.1 t2 done

---
+++ Project/OrientationTask1_2/src/t1/Event.java
@@ -0,0 +1,29 @@
+package t1;
+
+public class Event implements Comparable<Event> {
+    private int eventTime;
+    private String eventType;
+
+
+
+    public Event(int eventTime, String eventType,EventList eventList) {
+        this.eventTime= eventTime;
+        this.eventType = eventType;
+       eventList.addToEventList(this);
+    }
+    @Override
+    public int compareTo(Event other) {
+        return Integer.compare(this.eventTime, other.eventTime);// compare event times and set them to order
+    }
+    public int getEventTime() {
+        return eventTime;
+
+    }
+    public String getEventType() {
+        return eventType;
+    }
+
+
+}
+
+
--- Project/OrientationTask1_2/src/t1/Main_t1.java
+++ Project/OrientationTask1_2/src/t1/Main_t1.java
@@ -1,4 +1,23 @@
+//(P) Define the Event and EventList classes so that the EventList can be used to get the Event objects processed in the
+//correct chronological order. The Event is associated with an event time as an essential piece of information, and
+// it is essential that the next event in the event list can be retrieved on demand.
+//To implement the list, you can use the PriorityQueue class so that the EventList contains a PriorityQueue.
+// To implement the ordering, Events must implement the Comparable interface, see below.
+//Make a test program to a) generate a list of events, b) remove the first (=next to be processed) event from the list,
+// and c) print the contents of the EventList, ordered by event time. As a tip for c: The API description says that
+// Java's for-iteration does not print the contents of the PriorityQueueue in any particular order, but you have to
+// find a way other than for-iteration.
+
 package t1;
 
 public class Main_t1 {
+    public static void main(String[] args){
+        EventList eventList = new EventList();
+
+        Event event1 = new Event(3,"C",eventList);
+        Event event2 = new Event(5,"B",eventList);
+        Event event3 = new Event(9,"A",eventList);
+
+    eventList.printEventList();//print evenlist in priority order
+    }
 }
