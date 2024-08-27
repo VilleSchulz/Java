1a1a148ed2ce3aaf656d2008548ae3a56495ee1e
Ville <villesc@gmail.com>
2024-08-27 20:28:07 +0300

    Orientation 1.1 t06 modded

--- Project/OrientationTask1_1/src/t6/Main_t6.java
+++ Project/OrientationTask1_1/src/t6/Main_t6.java
@@ -4,7 +4,6 @@
 package t6;
 
 import java.util.Scanner;
-import static t6.ServicePoint.totalServiceTime;
 public class Main_t6 {
     public static boolean testFlag = true;// test flag => true for testing simulation multiple times
     public static int simulationAmmount;
