224af3c80d6064f64ada47b543c3947da44a4a8a
VilleSchulz <villemsc@metropolia.fi>
2024-08-29 17:23:19 +0300

    m2.2 t02 started

--- m2_2/src/GroceryListManager_t02.java
+++ m2_2/src/GroceryListManager_t02.java
@@ -1,11 +1,13 @@
-import java.util.ArrayList;
-
-
-
+////Task 2: Total Cost Calculation
+////Modify the GroceryListManager class to store both the item name and its corresponding cost as a pair
+//// (e.g., using a HashMap<String, Double>). Add the following methods:
+////addItem(String item, double cost): This method should add the given item and its cost to the grocery list.
+////calculateTotalCost(): This method should calculate and return the total cost of all items in the grocery list.
 
 import java.util.ArrayList;
 
 public class GroceryListManager_t02 {
+
     private ArrayList<String> groceryList = new ArrayList<>();
 
 
@@ -14,7 +16,7 @@ public class GroceryListManager_t02 {
     }
 
     public void removeItem(String item) {
-        System.out.println("Removing " + item);
+        System.out.println("Removing "+ item);
         groceryList.remove(item);
     }
 
@@ -29,11 +31,34 @@ public class GroceryListManager_t02 {
         }
 
     }
-
     public boolean checkItem(String item) {
-        System.out.print("Is " + item + " in grocery list? ");
+        System.out.print("Is "+item+" in grocery list? ");
         return groceryList.contains(item);
     }
-}
+    public static void main_() {
+        GroceryListManager_t02 groceryListManager = new GroceryListManager_t02();
+        groceryListManager.addItem("Apple");
+        groceryListManager.addItem("Milk");
+        groceryListManager.addItem("Beer");
+        System.out.println(groceryListManager.checkItem("Milk"));
 
+        groceryListManager.removeItem("Apple");
+        groceryListManager.displayList();
+
+        groceryListManager.removeItem("Milk");
+
+        groceryListManager.displayList();
+
+        groceryListManager.removeItem("Beer");
+        groceryListManager.displayList();
+
+    }
+    public static void main(String[] args){
+        GroceryListManager_t02.main_();
+
+    }
+
+
+
+}
 
