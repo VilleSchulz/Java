94f4bb161a2c1c00ad77c34cf1cf703118907bc8
Ville <villesc@gmail.com>
2024-09-02 19:37:23 +0300

    m2_3 t01 done

---
+++ m2_3/src/t01/Library.java
@@ -0,0 +1,41 @@
+package t01;
+
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> books = new ArrayList<>();
+
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void displayBooks() {
+        int counter = 0;
+        System.out.println("****Library Catalog****");
+        for (Book book : books) {
+            counter++;
+            System.out.println(counter + ". " + "Title: " + '"' + book.getTitle() + '"' + " Author: " + '"' + book.getAuthor() + '"' +
+                    ", Year: " +
+                    book.getPublicationYear() +
+                    "\n*******************");
+
+        }
+
+
+    }
+
+    public void findBooksBYAuthor(String author) {
+        System.out.println("Books by author " +'"'+ author+'"'+":");
+        for (Book book : books) {
+            if (book.getAuthor().equals(author)) {
+                System.out.println(
+                        "Title: " + book.getTitle() + " , year: " +
+                                book.getPublicationYear() +
+                                "\n*******************");
+
+            }
+        }
+    }
+}
+
---
+++ m2_3/src/t01/Main_t01.java
@@ -0,0 +1,18 @@
+package t01;
+
+public class Main_t01 {
+    public static void main(String[] args){
+        Library lib = new Library();
+        Book Book1 = new Book("Kauna","Jari",1998);
+        Book Book2 = new Book("Kauna2","Jari",1999);
+        Book Book3 = new Book("Kauna3","Jari3",2000);
+        Book Book4 = new Book("Kauna4","Jari2",2020);
+        lib.addBook(Book1);
+        lib.addBook(Book2);
+        lib.addBook(Book3);
+        lib.addBook(Book4);
+        lib.findBooksBYAuthor("Jari");
+        lib.findBooksBYAuthor("Jari2");
+        lib.displayBooks();
+    }
+}
