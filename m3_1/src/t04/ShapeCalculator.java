cb8dadad69b0cc43baf3d68e3e05cb295dd22c08
VilleSchulz <villemsc@metropolia.fi>
2024-09-05 10:41:10 +0300

    3.2 t01 started

--- m3_1/src/t04/ShapeCalculator.java
+++ m3_1/src/t04/ShapeCalculator.java
@@ -8,6 +8,7 @@ public class ShapeCalculator {
         shapes[0] = new Circle(20, "blue");
         shapes[1] = new Rectangle(22, 33, "red");
         shapes[2] = new Triangle(22, 33,"yellow");
+        System.out.println("Shape Calculator\n");
         for (Shape shape : shapes) {
             shape.calculateArea();
         }
