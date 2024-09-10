91151c1d724a00f2bcd5a5387bfe5ae1b9a614ac
Ville <villesc@gmail.com>
2024-09-10 14:52:45 +0300

    orientation 1.3 t02

---
+++ Project/OrientationTask1_3/src/t02/AgeDistribution.java
@@ -0,0 +1,39 @@
+package t02;
+public class AgeDistribution {
+    public static void main(String[] args) {
+        final int REITERATIONS = 36;
+        final int MAXAGE = 40;
+
+        int agedistribution[][] = {
+                {16, 20},  		// 16 %		// This line matches to random numbers 1..16,  gives age 20
+                {34, 21},  		// 18 %    	// This line matches to random numbers 17..34, gives age 21
+                {52, 22},		// 18 %    	// ...
+                {68, 23},    	// 16 %
+                {82, 24},  		// 14 %
+                {92, 27}, 		// 10 %
+                {96, 29},  		// 4 %
+                {98, 30},		// 2 %
+                {99, 33},  		// 1 %
+                {100,MAXAGE}, 	// 2 %	  	// This line matches to random numbers 99 ja 100
+        };
+
+        int generatedAges[] = new int[MAXAGE+1];
+
+        // Generate ages according to the distribution:
+        for (int i = 1; i <= REITERATIONS; i++){
+            int x = (int)(Math.random()*100)+1; 	// generate a random number 1..100 -> we get the row which gives the age
+            int j = 0;
+            while (x > agedistribution[j][0]) j++;  // search for the correct row to get the matching age
+            generatedAges[agedistribution[j][1]]++; // age founded gives an index to the generated ages count table: increment it (by one)
+        }
+
+        // Output the result of the generation:
+        System.out.println("Age  count     %-share");
+        for (int age = 0; age <= MAXAGE; age++) {
+            if (generatedAges[age] != 0) {
+                String str = "%-4d %-8d %-8.2f\n";
+                System.out.printf(str, age, generatedAges[age], ((double)generatedAges[age])/REITERATIONS*100 );
+            }
+        }
+    }
+}
