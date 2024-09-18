4a19912b313190bbefa9cc6795b47cedf15fce9e
Ville <villesc@gmail.com>
2024-09-18 23:56:17 +0300

    5.2 task 1 done

--- m5_2/src/t01/Main.java
+++ m5_2/src/t01/Main.java
@@ -4,21 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ReservationSystem reservationSystem = new ReservationSystem();
         reservationSystem.addTickets(100);
-        Thread[] threads = new Thread[200];
-        for (int i = 0; i < 200; i++) {
-            Customer customer = new Customer(reservationSystem.ticketList);
-            threads[i] = new Thread(customer);
-            threads[i].start();
-        }
-
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
+        reservationSystem.addCustomer(200);
         System.out.println("Tickets left: " + reservationSystem.getAvailableCapacity());
     }
 }
--- m5_2/src/t01/ReservationSystem.java
+++ m5_2/src/t01/ReservationSystem.java
@@ -5,27 +5,24 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class ReservationSystem {
     CopyOnWriteArrayList<Customer> customerList = new CopyOnWriteArrayList<>();
     CopyOnWriteArrayList<Ticket> ticketList = new CopyOnWriteArrayList<>();
-   /*
-   Ei toiminut täällä:(
-   public void addCustomer(int ammount) {
-        for (int i = 0; i < ammount; i++) {
-            Customer customer = new Customer(ticketList);
-            Thread t = new Thread(customer);//lisää customeri omaa threadiin.
-            customerList.add(customer);
-            t.start();
-            try{
-                t.join();
-            }
-            catch (InterruptedException e)
-            {
-                e.printStackTrace();
-            }
 
+   public void addCustomer(int amount) {
+       Thread[] threads = new Thread[amount];
+       for (int i = 0; i < amount; i++) {
+           Customer customer = new Customer(ticketList);
+           threads[i] = new Thread(customer);
+           customerList.add(customer);
+           threads[i].start();
+       }
 
-
-
-        }
-    }*/
+       for (Thread thread : threads) {
+           try {
+               thread.join();
+           } catch (InterruptedException e) {
+               e.printStackTrace();
+           }
+       }
+   }
     public void addTickets(int ammount) {
         for (int i = 0; i < ammount; i++) {
             Ticket ticket = new Ticket();
