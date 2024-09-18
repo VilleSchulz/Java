95f97929efc97d727265825650522ef2dd8b20be
Ville <villesc@gmail.com>
2024-09-18 23:57:25 +0300

    5.2 task 1 done

--- m5_2/src/t01/Main.java
+++ m5_2/src/t01/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ReservationSystem reservationSystem = new ReservationSystem();
         reservationSystem.addTickets(100);
-        reservationSystem.addCustomer(200);
+        reservationSystem.addCustomer(200);//täällä lisätään thredaus(käyttäjät ovat ns lippujonossa)
         System.out.println("Tickets left: " + reservationSystem.getAvailableCapacity());
     }
 }
--- m5_2/src/t01/ReservationSystem.java
+++ m5_2/src/t01/ReservationSystem.java
@@ -5,7 +5,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class ReservationSystem {
     CopyOnWriteArrayList<Customer> customerList = new CopyOnWriteArrayList<>();
     CopyOnWriteArrayList<Ticket> ticketList = new CopyOnWriteArrayList<>();
-
+//tässä tehdään thredaus kun lisätää käyttäjät "lippujonoon"
    public void addCustomer(int amount) {
        Thread[] threads = new Thread[amount];
        for (int i = 0; i < amount; i++) {
