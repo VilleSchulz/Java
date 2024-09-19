8ffabb5b1eb01676275510f50c195ffed29a1be3
VilleSchulz <villemsc@metropolia.fi>
2024-09-19 10:10:34 +0300

    5.1 modded t02modded

--- m5_1/src/t02/ParellelNum.java
+++ m5_1/src/t02/ParellelNum.java
@@ -22,34 +22,34 @@ public class ParellelNum implements Runnable {
     @Override
     public void run() {
         int sum = 0;
-        for (int i = startIndex; i < endIndex; i++) {
+        for (int i = startIndex; i < endIndex; i++) {//calculates the sum of the array
             sum += numberArray[i];
         }
-        endTime = System.nanoTime();
-        double time = (endTime - startTime);
+        endTime = System.nanoTime();//set end time for calculation
+        double time = (endTime - startTime);//calculate time taken
         System.out.println("Core " +coreNumber+" Sum of the array is: " + sum+ " Time taken: "+time+" ns");
     }
 
     public static void main(String[] args) {
-        final int numberArray[] = new int[100000];
+        final int numberArray[] = new int[100000];//create array of 100000
         Random random = new Random();
-        for (int i = 0; i < numberArray.length; i++) {
+        for (int i = 0; i < numberArray.length; i++) {//fill array with random numbers
             numberArray[i] = random.nextInt(10000);
         }
-            int cores = Runtime.getRuntime().availableProcessors();
-            int indexLenght = numberArray.length / cores;
+            int cores = Runtime.getRuntime().availableProcessors();//check how many cores the computer has
+            int indexLenght = numberArray.length / cores;//calculate how many indexes each thread should process
             int startIndex = 0;
-            int endIndex = indexLenght;
-            int coreNumber = 1;
-            double startTime = System.nanoTime();
+            int endIndex = indexLenght;//set end index for the first thread
+            int coreNumber = 1;//start core number from 1
+            double startTime = System.nanoTime();//set start time for calculation
             for (int j = 0; j < cores; j++) {
-                if (j == cores - 1) {
+                if (j == cores - 1) {//check if it is the last thread
                     endIndex = numberArray.length; // Ensure the last thread processes until the end of the array
                 }
                 Thread t = new Thread(new ParellelNum(startTime,coreNumber,numberArray, startIndex, endIndex));
-                startIndex += indexLenght;
-                endIndex += indexLenght;
-                coreNumber++;
+                startIndex += indexLenght;//set start index for the next thread
+                endIndex += indexLenght;//set end index for the next thread
+                coreNumber++;//add one to core number
                 t.start();
             }
 
